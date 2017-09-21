10fcf24f1d9e9371cfd1799f7c01e97b9d8e7401
foozie3moons <matthew.bell.290@gmail.com>
2017-09-21 15:43:17 -0700

    converted to pug and connected image upload with cloudinary

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
+require('dotenv').config();
 var express = require('express');
-var ejsLayouts = require('express-ejs-layouts');
+var multer = require('multer');
+var cloudinary = require('cloudinary');
+var upload = multer({dest: './uploads/'});
 var app = express();
 
-app.set('view engine', 'ejs');
-app.use(ejsLayouts);
+app.set('view engine', 'pug');
+app.set('view options', {
+  layout: false // pug has default layout functionality
+});
+
+var images = [];
 
 app.get('/', function(req, res) {
-  res.render('index');
+  res.render('index', {images, cloudinary});
+});
+
+app.post('/', upload.single('file'), function(req, res) {
+  cloudinary.uploader.upload(req.file.path, function(result) {
+    images.push(result.public_id);
+    res.redirect('/');
+  });
 });
 
 app.listen(3000);
